0bb862f2e1848d1370ea46388daba0f09ea95b7c
ali-0111 <alirahanet@gmail.com>
2024-06-15 00:36:11 +0430

    Refactored db schema

--- schema.sql
+++ schema.sql
@@ -14,41 +14,47 @@ CREATE TABLE nurse (
 CREATE TABLE child (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    father_name VARCHAR(255) NOT NULL,
-    mother_name VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    province VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    contact_number VARCHAR(20) NOT NULL
+    nurse_id INTEGER NOT NULL,
+    hospital_id INTEGER NOT NULL,
+    FOREIGN KEY (nurse_id) REFERENCES nurse(id),
+    FOREIGN KEY (hospital_id) REFERENCES hospital(id)
 );
 
-CREATE TABLE vaccine_type (
+CREATE TABLE vaccine (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE vaccination_program (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    step INT NOT NULL
+    number_of_steps INT NOT NULL
 );
 
-CREATE TABLE vaccine_step (
+CREATE TABLE vaccination_program_step(
     id SERIAL PRIMARY KEY,
-    vaccine_type_id INT NOT NULL,
-    step_number INT NOT NULL,
+    vaccination_program_id INT NOT NULL,
+    step_rank INT NOT NULL,
     step_name VARCHAR(255) NOT NULL,
-    step_timing INTERVAL NOT NULL,
-    FOREIGN KEY (vaccine_type_id) REFERENCES vaccine_type(id)
-);
+    step_status VARCHAR(50) NOT NULL,
+    vaccine_id INT NOT NULL,
+    FOREIGN KEY (vaccination_program_id) REFERENCES vaccination_program(id),
+    FOREIGN KEY (vaccine_id) REFERENCES vaccine(id) 
+)
 
-CREATE TABLE child_vaccine_progress (
-    id SERIAL PRIMARY KEY,
-    child_id INT NOT NULL,
-    hospital_id INT NOT NULL,
-    nurse_id INT NOT NULL,
-    step_number INT NOT NULL,
-    vaccine_type_id INT NOT NULL,
-    vaccine_timed_at TIMESTAMP,
-    time_taken INTERVAL,
-    status VARCHAR(50) NOT NULL,
-    FOREIGN KEY (child_id) REFERENCES child(id),
-    FOREIGN KEY (hospital_id) REFERENCES hospital(id),
-    FOREIGN KEY (nurse_id) REFERENCES nurse(id),
-    FOREIGN KEY (vaccine_type_id) REFERENCES vaccine_type(id)
-);
+
+-- CREATE TABLE child_vaccine_progress (
+--     id SERIAL PRIMARY KEY,
+--     child_id INT NOT NULL,
+--     hospital_id INT NOT NULL,
+--     nurse_id INT NOT NULL,
+--     step_number INT NOT NULL,
+--     vaccine_id INT NOT NULL,
+--     vaccine_timed_at TIMESTAMP,
+--     time_taken INTERVAL,
+--     status VARCHAR(50) NOT NULL,
+--     FOREIGN KEY (child_id) REFERENCES child(id),
+--     FOREIGN KEY (hospital_id) REFERENCES hospital(id),
+--     FOREIGN KEY (nurse_id) REFERENCES nurse(id),
+--     FOREIGN KEY (vaccine_id) REFERENCES vaccine(id)
+-- );
