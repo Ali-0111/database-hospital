5f899a9bd3dc7c73177435cbd6dc6bec0ac5f278
ali-0111 <alirahanet@gmail.com>
2024-06-20 10:27:59 +0430

    Added table vaccine_record

--- schema.sql
+++ schema.sql
@@ -36,25 +36,19 @@ CREATE TABLE vaccination_program_step(
     vaccination_program_id INT NOT NULL,
     step_rank INT NOT NULL,
     step_name VARCHAR(255) NOT NULL,
-    step_status VARCHAR(50) NOT NULL,
     vaccine_id INT NOT NULL,
     FOREIGN KEY (vaccination_program_id) REFERENCES vaccination_program(id),
     FOREIGN KEY (vaccine_id) REFERENCES vaccine(id) 
-)
+);
 
-
--- CREATE TABLE child_vaccine_progress (
---     id SERIAL PRIMARY KEY,
---     child_id INT NOT NULL,
---     hospital_id INT NOT NULL,
---     nurse_id INT NOT NULL,
---     step_number INT NOT NULL,
---     vaccine_id INT NOT NULL,
---     vaccine_timed_at TIMESTAMP,
---     time_taken INTERVAL,
---     status VARCHAR(50) NOT NULL,
---     FOREIGN KEY (child_id) REFERENCES child(id),
---     FOREIGN KEY (hospital_id) REFERENCES hospital(id),
---     FOREIGN KEY (nurse_id) REFERENCES nurse(id),
---     FOREIGN KEY (vaccine_id) REFERENCES vaccine(id)
--- );
+CREATE TABLE vaccine_record (
+    id SERIAL PRIMARY KEY,
+    child_id INT NOT NULL REFERENCES child(id),
+    nurse_id INT NOT NULL REFERENCES nurse(id),
+    hospital_id INT NOT NULL REFERENCES hospital(id),
+    vaccination_program_id INT NOT NULL REFERENCES vaccination_program(id),
+    vaccine_id INT NOT NULL REFERENCES vaccine(id),
+    step_rank INT NOT NULL,
+    step_status VARCHAR(50) NOT NULL,
+    vaccination_date TIMESTAMP
+);
